f6b3218f8c35209761c78498c450d096f2760964
DarshitJain04 <jain.38@iitj.ac.in>
2023-04-22 22:56:37 +0530

    fixed DOM query in popup.js

--- background.js
+++ background.js
@@ -1,58 +1,65 @@
 async function checkPhishing(emailContent) {
-  const response = await fetch('http://127.0.0.1:5000/check_phishing', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email: emailContent }),
-  });
+  // const response = await fetch('http://127.0.0.1:5000/check_phishing', {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ email: emailContent }),
+  // });
 
-  const result = await response.json();
-  return jsonify({
-    "result": "phishing" 
-  }) 
+  // const result = await response.json();
+  console.log('checkPhishing() called');
+  return JSON.stringify({
+    "result": "phishing"
+  })
 }
 
 async function summarizeEmail(emailContent) {
-  const response = await fetch('http://127.0.0.1:5000/summarize', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email: emailContent }),
-  });
+  // const response = await fetch('http://127.0.0.1:5000/summarize', {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ email: emailContent }),
+  // });
 
-  const summary = await response.json();
-  return summary;
+  // const summary = await response.json();
+  // return summary;
+  console.log('summarizeEmail() called');
+  return JSON.stringify({
+    "result": "summarize"
+  })
 }
 
 async function getAccessToken() {
   return new Promise((resolve, reject) => {
-      chrome.identity.getAuthToken({ interactive: true }, (token) => {
-          if (chrome.runtime.lastError) {
-              reject(chrome.runtime.lastError);
-          } else {
-              resolve(token);
-          }
-      });
+    chrome.identity.getAuthToken({ interactive: true }, (token) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(token);
+      }
+    });
   });
 }
 
 async function getEmailContent() {
-  const accessToken = await getAccessToken();
-  const response = await fetch('https://www.googleapis.com/gmail/v1/users/me/messages?q=in:inbox', {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  const data = await response.json();
-  const emailId = data.messages[0].id;
+  // const accessToken = await getAccessToken();
+  // const response = await fetch('https://www.googleapis.com/gmail/v1/users/me/messages?q=in:inbox', {
+  //   headers: {
+  //     Authorization: `Bearer ${accessToken}`,
+  //   },
+  // });
+  // const data = await response.json();
+  // const emailId = data.messages[0].id;
 
-  const emailResponse = await fetch(`https://www.googleapis.com/gmail/v1/users/me/messages/${emailId}`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  const emailData = await emailResponse.json();
-  const emailContent = emailData.snippet;
+  // const emailResponse = await fetch(`https://www.googleapis.com/gmail/v1/users/me/messages/${emailId}`, {
+  //   headers: {
+  //     Authorization: `Bearer ${accessToken}`,
+  //   },
+  // });
+  // const emailData = await emailResponse.json();
+  // const emailContent = emailData.snippet;
 
-  return emailContent;
+  // return emailContent;
+  console.log('getEmailContent() called');
+  return "qwertyuiopasdfghjklzxcvbnm";
 }
 
 
@@ -75,14 +82,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'checkPhishing') {
     getEmailContent().then(async (emailContent) => {
       const result = await checkPhishing(emailContent);
-      sendResponse({result: result});
+      sendResponse({ result: result });
     });
-    return true; 
+    return true;
   } else if (request.action === 'summarizeEmail') {
     getEmailContent().then(async (emailContent) => {
       const summary = await summarizeEmail(emailContent);
-      sendResponse({summary: summary});
+      sendResponse({ summary: summary });
     });
-    return true; 
+    return true;
   }
 });
--- popup.js
+++ popup.js
@@ -1,17 +1,13 @@
-chrome.runtime.onInstalled.addListener(() => {
-  document.getElementById('checkPhishing').addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: 'checkPhishing' }, (response) => {
-      document.getElementById('result').innerText = `Phishing check result: ${response.result}`;
-    });
+document.getElementById('checkPhishing').addEventListener('click', () => {
+  console.log('phi clicked');
+  chrome.runtime.sendMessage({ action: 'checkPhishing' }, (response) => {
+    document.getElementById('result').innerText = `Phishing check result: ${response.result}`;
   });
 });
 
 
-chrome.runtime.onInstalled.addListener(() => {
-  document.getElementById('summarizeEmail').addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: 'summarizeEmail' }, (response) => {
-      //document.getElementById('result').innerText = `Summarization result: ${response.result}`;
-      console.log(response);
-    });
+document.getElementById('summarizeEmail').addEventListener('click', () => {
+  chrome.runtime.sendMessage({ action: 'summarizeEmail' }, (response) => {
+    document.getElementById('result').innerText = `Summarization result: ${response.summary}`;
   });
 });
